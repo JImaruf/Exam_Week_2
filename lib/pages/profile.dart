bd9320b1a5d78474d5719f9afdde14be5dce52c8
JImaruf <jimaruf1@gamil.com>
2024-04-30 23:42:35 +0600

    commiting

---
+++ lib/pages/profile.dart
@@ -0,0 +1,35 @@
+import 'package:exam_week_2/widgets/landscape.dart';
+import 'package:exam_week_2/widgets/portrait.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile',style: TextStyle(fontSize: 24,color: Colors.black,fontWeight: FontWeight.bold),),
+        centerTitle: true,
+      ),
+
+      body:
+      OrientationBuilder(builder: (context, orientation) {
+        if(orientation==Orientation.portrait)
+          {
+            return
+          portrait();
+          }
+        else
+          {
+            return
+            landScape();
+          }
+
+      },)
+
+    );
+
+
+  }
+}
